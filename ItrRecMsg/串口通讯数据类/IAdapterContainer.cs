fbc4b1cae0c5c1a981528b6c6a93216aa38b77b0
FeedFish34 <1073452867@qq.com>
2019-08-16 14:48:35 +0800

    First commit

---
+++ ItrRecMsg/串口通讯数据类/IAdapterContainer.cs
@@ -0,0 +1,15 @@
+﻿using FrameWrok.UI.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItrRecMsg
+{
+    public interface IAdapterContainer: IAppContainer
+    {
+        //void GenLog(ELogType LogType,string LogMessage);
+
+    }
+}
